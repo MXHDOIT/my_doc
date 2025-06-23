5c1a53bcc507ddffed5ec9d8709618780d86b2a2
Xinhang Ma <maxinhangdoit@163.com>
2025-06-23 20:45:27 +0800

    20250623.最长递增子序列

---
+++ arithmetic/src/leetcode/Solution_300.java
@@ -0,0 +1,24 @@
+package leetcode;
+
+import java.util.Arrays;
+
+/**
+ * @author: maxinhang.
+ */
+public class Solution_300 {
+    public int lengthOfLIS(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n];
+        Arrays.fill(dp, 1);
+        int max = 0;
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                    max = Math.max(max, dp[i]);
+                }
+            }
+        }
+        return max;
+    }
+}
