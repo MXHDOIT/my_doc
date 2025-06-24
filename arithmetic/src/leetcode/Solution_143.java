f3a9f77ef01153b9104615a8fbd8590075b14fb6
Xinhang Ma <maxinhangdoit@163.com>
2025-06-24 19:38:37 +0800

    20250624.重排链表

---
+++ arithmetic/src/leetcode/Solution_143.java
@@ -0,0 +1,35 @@
+package leetcode;
+
+import leetcode.model.ListNode;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * @author: maxinhang.
+ */
+public class Solution_143 {
+    public void reorderList(ListNode head) {
+        if (head == null) {
+            return;
+        }
+        List<ListNode> list = new ArrayList<ListNode>();
+        ListNode node = head;
+        while (node != null) {
+            list.add(node);
+            node = node.next;
+        }
+        int i = 0, j = list.size() - 1;
+        while (i < j) {
+            list.get(i).next = list.get(j);
+            i++;
+            if (i == j) {
+                break;
+            }
+            list.get(j).next = list.get(i);
+            j--;
+        }
+        list.get(i).next = null;
+    }
+}
