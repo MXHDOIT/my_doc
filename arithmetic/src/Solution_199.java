330e3a3a44b1906236e68c83e8fead8de9c0cddf
Xinhang Ma <maxinhangdoit@163.com>
2025-07-30 11:24:04 +0800

    20250730.二叉树的右视图

---
+++ arithmetic/src/Solution_199.java
@@ -0,0 +1,35 @@
+import leetcode.model.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * @author: maxinhang.
+ */
+public class Solution_199 {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if (root == null) return result;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            Queue<TreeNode> temp = new LinkedList<>();
+            while (!queue.isEmpty()) {
+                TreeNode treeNode = queue.poll();
+                if (treeNode.left != null) {
+                    temp.offer(treeNode.left);
+                }
+                if (treeNode.right != null) {
+                    temp.offer(treeNode.right);
+                }
+                if (queue.isEmpty()) {
+                    result.add(treeNode.val);
+                }
+            }
+            queue = temp;
+        }
+        return result;
+    }
+}
---
+++ arithmetic/src/leetcode/Solution_4.java
@@ -0,0 +1,11 @@
+package leetcode;
+
+/**
+ * @author: maxinhang.
+ */
+public class Solution_4 {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        //TODO
+        return 0.0D;
+    }
+}
