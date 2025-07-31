fc1e7e8020eeb32f940153bb5158ff2a81ba059d
Xinhang Ma <maxinhangdoit@163.com>
2025-07-31 11:32:13 +0800

    20250731.二叉树的中序遍历

---
+++ arithmetic/src/leetcode/Solution_94.java
@@ -0,0 +1,46 @@
+package leetcode;
+
+import leetcode.model.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * @author: maxinhang.
+ */
+public class Solution_94 {
+
+    public List<Integer> inorderTraversal1(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+
+        while (root != null || !stack.isEmpty()) {
+            if (root != null) {
+                stack.push(root);
+                root = root.left;
+            } else {
+                TreeNode node = stack.pop();
+                list.add(node.val);
+                root = node.right;
+            }
+        }
+
+        return list;
+    }
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        inorderTraversal(root, list);
+        return list;
+    }
+
+    public void inorderTraversal(TreeNode root, List<Integer> list) {
+        if (root == null) {
+            return;
+        }
+        inorderTraversal(root.left, list);
+        list.add(root.val);
+        inorderTraversal(root.right, list);
+    }
+}
