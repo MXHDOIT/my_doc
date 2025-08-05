760073cf6e945651fdf3ae39fd72cc21d0162af8
Xinhang Ma <maxinhangdoit@163.com>
2025-08-05 22:19:53 +0800

    20250805.和为K的子数组

--- arithmetic/src/leetcode/Solution_560.java
+++ arithmetic/src/leetcode/Solution_560.java
@@ -1,9 +1,41 @@
 package leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author: maxinhang.
  */
 public class Solution_560 {
+
+    public int subarraySum(int[] nums, int k) {
+        int s = 0;
+        int ans = 0;
+        Map<Integer, Integer> cnt = new HashMap<>();
+        cnt.put(0, 1);
+        for (int num : nums) {
+            s += num;
+            ans += cnt.getOrDefault(s - k, 0);
+            cnt.merge(s, 1, Integer::sum);
+        }
+        return ans;
+    }
+
+    public int subarraySum2(int[] nums, int k) {
+        int n = nums.length;
+        int[] prefix = new int[n + 1];
+        for (int i = 0; i < n; i++) {
+            prefix[i + 1] = prefix[i] + nums[i];
+        }
+
+        int ans = 0;
+        Map<Integer, Integer> cnt = new HashMap<>();
+        for (int num : prefix) {
+            ans = cnt.getOrDefault(num - k, 0);
+            cnt.merge(num, 1, Integer::sum);
+        }
+        return ans;
+    }
 
     public int subarraySum1(int[] nums, int k) {
         int n = nums.length;
