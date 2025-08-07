1d8bcd6067c0110a9347dd09e0dcbb2a6dc707dc
Xinhang Ma <maxinhangdoit@163.com>
2025-08-07 11:01:24 +0800

    20250807.除自身以外数组的乘积

---
+++ arithmetic/src/leetcode/Solution_238.java
@@ -0,0 +1,21 @@
+package leetcode;
+
+/**
+ * @author: maxinhang.
+ */
+public class Solution_238 {
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[n];
+        ans[n - 1] = 1;
+        for (int i = n - 2; i >= 0; i--) {
+            ans[i] = nums[i + 1] * ans[i + 1];
+        }
+        int prefix = 1;
+        for (int i = 0; i < n; i++) {
+            ans[i] = prefix * ans[i];
+            prefix *= nums[i];
+        }
+        return ans;
+    }
+}
