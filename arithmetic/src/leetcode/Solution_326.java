60317db6aaf7c1d59441604ca9a30f3ba1848fff
Xinhang Ma <maxinhangdoit@163.com>
2025-08-13 14:12:47 +0800

    20250813.3的幂

---
+++ arithmetic/src/leetcode/Solution_326.java
@@ -0,0 +1,13 @@
+package leetcode;
+
+/**
+ * @author: maxinhang.
+ */
+public class Solution_326 {
+    public boolean isPowerOfThree(int n) {
+        while (n != 0 && n % 3 == 0) {
+            n /= 3;
+        }
+        return n == 1;
+    }
+}
