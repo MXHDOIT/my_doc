6d7772ade8f10d603e91a64df8be164e79fb341b
Xinhang Ma <maxinhangdoit@163.com>
2025-08-18 20:37:59 +0800

    20250818.零钱兑换

---
+++ arithmetic/src/leetcode/Solution_322.java
@@ -0,0 +1,28 @@
+package leetcode;
+
+import java.util.Arrays;
+
+/**
+ * @author: maxinhang.
+ */
+public class Solution_322 {
+
+    public static void main(String[] args) {
+        new Solution_322().coinChange(new int[]{2}, 3);
+    }
+
+    public int coinChange(int[] coins, int amount) {
+        int max = amount + 1;
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, max);
+        dp[0] = 0;
+        for (int i = 1; i <= amount; i++) {
+            for (int j = 0; j < coins.length; j++) {
+                if (coins[j] <= i) {
+                    dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
+                }
+            }
+        }
+        return dp[amount] > amount ? -1 : dp[amount];
+    }
+}
