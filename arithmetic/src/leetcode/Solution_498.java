5448ba0e99c1b3eb85fe3750c4352d1606418ea5
Xinhang Ma <maxinhangdoit@163.com>
2025-08-25 15:02:10 +0800

    20250825.对角线遍历

---
+++ arithmetic/src/leetcode/Solution_498.java
@@ -0,0 +1,66 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author: maxinhang.
+ */
+public class Solution_498 {
+
+    public int[] findDiagonalOrderOpt(int[][] mat) {
+        int n = mat.length;
+        int m = mat[0].length;
+        int[] ans = new int[n * m];
+        int index = 0;
+        for (int i = 0; i < n + m - 1; i++) {
+            if (i % 2 == 0) {
+                int x = i < n ? i : n - 1;
+                int y = i < n ? 0 : i - n + 1;
+                while (x >= 0 && y < m) {
+                    ans[index++] = mat[x][y];
+                    x--;
+                    y++;
+                }
+            } else {
+                int x = i < m ? 0 : i - m + 1;
+                int y = i < m ? i : m - 1;
+                while (x < n && y >= 0) {
+                    ans[index++] = mat[x][y];
+                    x++;
+                    y--;
+                }
+            }
+        }
+        return ans;
+    }
+
+    public int[] findDiagonalOrder(int[][] mat) {
+        int n = mat.length;
+        int m = mat[0].length;
+        int length = n + m - 1;
+        List<List<Integer>> list = new ArrayList<>(length);
+        for (int i = 0; i < length; i++) {
+            list.add(new ArrayList<>());
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                list.get(i + j).add(mat[i][j]);
+            }
+        }
+        int[] ans = new int[n * m];
+        boolean reverse = true;
+        int index = 0;
+        for (List<Integer> nums : list) {
+            if (reverse) {
+                Collections.reverse(nums);
+            }
+            for (Integer num : nums) {
+                ans[index++] = num;
+            }
+            reverse = !reverse;
+        }
+        return ans;
+    }
+}
