b61fee81290c6e4d2524c558c759286fdea8027f
Xinhang Ma <maxinhangdoit@163.com>
2025-09-12 10:12:05 +0800

    20250912.跳跃游戏

---
+++ arithmetic/src/leetcode/Solution_55.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * @author: maxinhang.
+ */
+public class Solution_55 {
+    public boolean canJump(int[] nums) {
+        int k = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (i > k) return false;
+            k = Math.max(k, i + nums[i]);
+        }
+        return true;
+    }
+}
