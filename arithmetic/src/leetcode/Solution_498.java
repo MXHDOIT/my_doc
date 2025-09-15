0bf6f89f9aa0465cc152026e18046a4aab291d72
Xinhang Ma <maxinhangdoit@163.com>
2025-09-15 19:53:00 +0800

    20250915.对角线遍历

--- arithmetic/src/leetcode/Solution_498.java
+++ arithmetic/src/leetcode/Solution_498.java
@@ -9,6 +9,32 @@ import java.util.List;
  */
 public class Solution_498 {
 
+    public int[] findDiagonalOrderOpt1(int[][] mat) {
+        int m = mat.length;
+        int n = mat[0].length;
+        int[] ans = new int[m * n];
+        int tier = m + n - 1;
+        int index = 0;
+        for (int i = 0; i < tier; i++) {
+            if (i % 2 == 0) {
+                //从下向上
+                int x = i < m ? i : m - 1;
+                int y = i < m ? 0 : i - m + 1;
+                while (x >= 0 && y < n) {
+                    ans[index] = mat[x--][y++];
+                }
+            } else {
+                //从上向下
+                int x = i < n ? 0 : i - n + 1;
+                int y = i < n ? i : n - 1;
+                while (x < m && y >= 0) {
+                    ans[index] = mat[x++][y--];
+                }
+            }
+        }
+        return ans;
+    }
+
     public int[] findDiagonalOrderOpt(int[][] mat) {
         int n = mat.length;
         int m = mat[0].length;
